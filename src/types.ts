3c950c48d0d582d8612a217fbc80626774b4937a
Jared Palmer <jared@palmer.net>
2019-01-10 21:25:13 -0500

    Add more stuff

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,14 @@ export type Omit<T, K extends keyof T> = Pick<
 
 export type PartialDeep<T> = { [P in keyof T]?: PartialDeep<T[P]> };
 export type Many<T> = T | T[];
+export interface Geo {
+  /**
+   * latitude
+   */
+  lat: number;
+
+  /**
+   * longitude
+   */
+  lng: number;
+}
